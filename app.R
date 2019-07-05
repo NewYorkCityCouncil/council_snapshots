121e8d71b62dc6cfa1a1d5210d4d18b9abd865bb
Nick Solomon <nsolomon@council.nyc.gov>
2019-07-05 18:16:02 -0400

    better week labels

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(shinydashboard)
 library(tibble)
 library(ggplot2)
 library(dplyr)
+library(lubridate)
 
 modules <- list.files(path = "modules", pattern = "\\.(R|r)$", full.names = TRUE)
 lapply(modules, source)
@@ -14,11 +15,21 @@ current_week <- tbl(snapshots_db, "sr_top_10_week_district") %>%
   summarize(week = max(week)) %>%
   pull()
 
+weeks <- tibble(week_n = 1:current_week) %>%
+  mutate(end_date = ceiling_date(ymd("2019-01-01"), unit = "week")+ 7*(week_n-1),
+         start_date = end_date - 6,
+         start_date = if_else(year(start_date) < year(end_date), ymd("2019-01-01"), start_date),
+         end_date = if_else(year(end_date) > year(start_date), ymd("2019-12-31"), end_date),
+         label = paste(format(start_date, format = "%b %e"), "-", format(end_date, format = "%b %e")) %>%
+           str_replace_all("\\s+", " "))
+week_labels <- weeks %>% pull(week_n)
+names(week_labels) <- weeks %>% pull(label)
+
 header <- dashboardHeader(title = "Council Snapshots")
 
 sidebar <- dashboardSidebar(
   selectInput("coun_dist", "Council district", 1:51, selected = 1),
-  selectInput("week", "Week", current_week:1, selected = current_week),
+  selectInput("week", "Week", week_labels, selected = current_week),
   sidebarMenu(
     menuItem("311", icon = icon("phone"),
              menuSubItem("First 311 page", "311_test")),
