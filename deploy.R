2ae184d835d0274a55d307764b36c2b4c9ab9c99
Nick Solomon <nsolomon@council.nyc.gov>
2019-08-15 16:43:48 -0400

    try this

--- deploy.R
+++ deploy.R
@@ -1,15 +1,16 @@
-library(git2r)
+# library(git2r)
+#
+# clean_name <- function(str) {
+#   gsub("[^a-zA-Z0-9_\\-]", "", str)
+# }
+#
+# app_name <- "council_snapshots"
+# branch_name <- repository_head()$name
+#
+# if(branch_name != "master") {
+#   rsconnect::deployApp(appName = clean_name(paste(app_name, "staging", branch_name, sep = "-")))
+# } else {
+#   rsconnect::deployApp(appName = app_name)
+# }
 
-clean_name <- function(str) {
-  gsub("[^a-zA-Z0-9_\\-]", "", str)
-}
-
-app_name <- "council_snapshots"
-branch_name <- repository_head()$name
-
-if(branch_name != "master") {
-  rsconnect::deployApp(appName = clean_name(paste(app_name, "staging", branch_name, sep = "-")))
-} else {
-  rsconnect::deployApp(appName = app_name)
-}
-
+rsconnect::deployApp(appName = "council_snapshots")
