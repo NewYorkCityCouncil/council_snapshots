3bfaa0aa7e012883fbc98d992ba69ba210ee135f
Nick Solomon <nsolomon@council.nyc.gov>
2019-08-16 17:46:52 -0400

    set up deployment

--- R/deploy.R
+++ R/deploy.R
@@ -1,32 +1,55 @@
-# library(git2r)
-#
-# clean_name <- function(str) {
-#   gsub("[^a-zA-Z0-9_\\-]", "", str)
-# }
-#
-# app_name <- "council_snapshots"
-# branch_name <- repository_head()$name
-#
-# if(branch_name != "master") {
-#   rsconnect::deployApp(appName = clean_name(paste(app_name, "staging", branch_name, sep = "-")))
-# } else {
-#   rsconnect::deployApp(appName = app_name)
-# }
-
-# rsconnect::deployApp(appName = "council_snapshots")
-
-#' Title
+#' Deploy to shinyapps.io from Travis
 #'
-#' @return
+#' @param host database hostname
+#' @param user database username
+#' @param password database password
+#' @param branchName branch being deployed from
+#' @param token rsconnect token
+#' @param secret rsconnect secret
+#'
 #' @export
-#'
-#' @examples
-deploy_shinyapps_io <- function() {
+deploy_shinyapps_io <- function(host = Sys.getenv("SNAPSHOTS_DB_HOST"),
+                                user = Sys.getenv("SNAPSHOTS_DB_HOST"),
+                                password = Sys.getenv("SNAPSHOTS_DB_PW"),
+                                branchName = Sys.getenv("TRAVIS_BRANCH"),
+                                token = Sys.getenv("RSCONNECT_TOKEN"),
+                                secret = Sys.getenv("RSCONNECT_SECRET")) {
 
-  writeLines(c(paste0("SNAPSHOTS_DB_HOST=", Sys.getenv("SNAPSHOTS_DB_HOST")),
-                   paste0("SNAPSHOTS_DB_USER=", Sys.getenv("SNAPSHOTS_DB_USER")),
-                   paste0("SNAPSHOTS_DB_PW=", Sys.getenv("SNAPSHOTS_DB_PW"))),
+  # Check for all needed info
+  if(any(c(host, user, password) == "")) {
+    stop("Missing database credentials")
+  }
+
+  if(branchName == "") {
+    stop("Missing name of deployment")
+  }
+
+  if(any(c(token, secret) == "")) {
+    stop("Missing rsconnect credentials")
+  }
+
+
+  # Create app name so I can stage branches
+  clean_name <- function(str) {
+    gsub("[^a-zA-Z0-9_\\-]", "", str)
+  }
+
+  if(branchName == "master") {
+    appName <- "council_snapshots"
+  } else{
+    appName <- clean_name(paste("council_snapshots", "staging", branchName, sep = "-"))
+  }
+
+  # Write app environment variables to local package directory
+  writeLines(c(paste0("SNAPSHOTS_DB_HOST=", host),
+               paste0("SNAPSHOTS_DB_USER=", user),
+               paste0("SNAPSHOTS_DB_PW=", password)),
              con = file.path(system.file("shinyApp", package = "councilsnapshots"), ".Renviron"))
 
-  rsconnect::deployApp(appDir = system.file("shinyApp", package = "councilsnapshots"))
+  # Deploy app to shinyapps.io
+  rsconnect::setAccountInfo(name='nycc',
+                            token=token,
+                            secret=secret)
+  rsconnect::deployApp(appDir = system.file("shinyApp", package = "councilsnapshots"),
+                       appName = appName)
 }
