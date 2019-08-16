898cf78065a0769129dfee3823f05b6b8b45360a
Nick Solomon <nsolomon@council.nyc.gov>
2019-08-16 14:27:45 -0400

    ad environment vars

--- R/deploy.R
+++ R/deploy.R
@@ -22,5 +22,12 @@
 #'
 #' @examples
 deploy_shinyapps_io <- function() {
+
+
+  writeLines(paste(paste0("SNAPSHOTS_DB_HOST=", Sys.getenv("SNAPSHOTS_DB_HOST")),
+                   paste0("SNAPSHOTS_DB_USER=", Sys.getenv("SNAPSHOTS_DB_USER")),
+                   paste0("SNAPSHOTS_DB_PW=", Sys.getenv("SNAPSHOTS_DB_PW")), sep = "\n"),
+             con = system.file("shinyApp/.Renviron", package = "councilsnapshots"))
+
   rsconnect::deployApp(appDir = system.file("shinyApp", package = "councilsnapshots"))
 }
