d424c62779da9b50030507040dcef050b5b17358
Nick Solomon <nsolomon@council.nyc.gov>
2019-09-09 11:51:06 -0400

    proposed layout tweaks

--- R/page_landing.R
+++ R/page_landing.R
@@ -13,38 +13,43 @@ page_landing_ui <- function(id) {
   fluidPage(
 
     fluidRow(
-      column(12, offset = 1,
+      column(11, offset = 1,
              tags$div(
                tags$span(style="font-size:600%; font-family:Georgia", "Council Snapshots"),
                tags$h2("An Open Data Plug to Your District")
              )
-            )
-      ),
+      )
+    ),
     hr(),
     fluidRow(
-      column(width = 2, offset = 1,
-             tags$a(style = "display:inline-block", class = 'sidebar-toggle btn btn-success btn-lg',
-                    `data-toggle` = 'offcanvas', role = 'button', 'Explore Now')
+      column(width = 3, offset = 1,
+             wellPanel(
+               tags$h3("Select your desired:"),
+               h4(tags$span(class="glyphicon glyphicon-chevron-down"), "Council District"),
+               h4(tags$span(class="glyphicon glyphicon-chevron-down"), "Week"),
+               h4(tags$span(class="glyphicon glyphicon-chevron-down"), "Dataset"),
+               div(style = "text-align:center;",
+                   tags$a(class = 'sidebar-toggle btn btn-success btn-lg',
+                          style = "display:inline-block;width:100%",
+                          `data-toggle` = 'offcanvas', role = 'button', 'Explore Now')
+               ))
       ),
       column(width = 3,
-             box(
-               tags$h4("Select your desired:"),
-               tags$span(h4(class="glyphicon glyphicon-chevron-down","Council District")),
-               br(),
-               tags$span(h4(class="glyphicon glyphicon-chevron-down","Week")),
-               br(),
-               tags$span(h4(class="glyphicon glyphicon-chevron-down","Dataset"))
-               )
-             ),
-      column(width = 3,
-             tags$h2(tags$ul(
+             wellPanel(
+               tags$h3(tags$ul(
                  tags$li("See where things are happening."),
                  tags$li("Track trends & make insights."),
                  tags$li("Be aware of top issues."),
                  tags$li("Dig deeper.")
-                 )
+               )
+               )
              )
-             )
+      ),
+      column(width = 3,
+             wellPanel(
+               h3("New Developments"), br(),
+               "Tentative Dates")
+      )
     ),
     hr(),
     fluidRow(
@@ -62,10 +67,10 @@ page_landing_ui <- function(id) {
                fluidRow(
                  column(width = 4,
                         tags$a(style= "display: center;", href="https://council.nyc.gov/data/",icon("globe", "fa-2x")), h6("Visit Us")
-                        ),
+                 ),
                  column(width = 4,
                         tags$a(href="https://twitter.com/nyccouncildata", icon("hashtag", "fa-2x")), h6("Follow Us")
-                        ),
+                 ),
                  column(width = 4,
                         tags$a(href="#", icon("envelope", "fa-2x")), h6("Email Us")
                  )
@@ -77,14 +82,6 @@ page_landing_ui <- function(id) {
                h3("Feedback"), br(), p("Let us know if you have any issues."),
                p("Tell us your suggestions for improvement.")
              )
-      )
-    ),
-    br(),
-    fluidRow(
-      column(width = 9, offset = 1,
-             wellPanel(
-               h3("New Developments"), br(),
-               "Tentative Dates")
       )
     )
   )
